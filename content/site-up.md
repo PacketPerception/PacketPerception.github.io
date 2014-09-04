2219ccaaed245d4fa7e3030288a0d7b93b15ebb9
Brian Knobbs <knobbsb@saic.com>
2014-09-03 23:49:07 -0400

    initial pelican

---
+++ content/site-up.md
@@ -0,0 +1,5 @@
+Title: Site Up
+Date: 2014-09-03 22:45
+Category: Random
+
+Packet Perception has moved to github pages
---
+++ pelicanconf.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Brian Knobbs'
+SITENAME = u'Packet Perception'
+SITEURL = ''
+
+PATH = 'content'
+
+THEME = 'pelican-themes/BT3-Flat'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+#WITH_FUTURE_DATES = False
+TEMPLATE_PAGES = {'blog.html': 'blog.html'}
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
+
+# Feed generation is usually not desired when developing
+
+# Feed generation is usually not desired when developing
+#FEED_RSS = 'feed/index.html'
+#FEED_ATOM = 'feed/atom/index.html'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),)
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/PacketPerception'),
+          ('envelope', 'mailto:brian@packetperception.com'))
+
+DEFAULT_PAGINATION = 10
+#PAGINATED_DIRECT_TEMPLATES = ('blog-index',)
+#DIRECT_TEMPLATES = ('categories', 'index', 'blog-index', 'blog')
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+#DEFAULT_DATE_FORMAT = ('%d/%b/%Y %a')
+
+# Formatting for urls
+#ARTICLE_URL = "{date:%Y}/{date:%m}/{slug}/"
+#ARTICLE_SAVE_AS = "{date:%Y}/{date:%m}/{slug}/index.html"
+
+# ===theme settings===========================
+POST_LIMIT = 3
+#FAVICON = 'https://dl.dropboxusercontent.com/u/299446/logo.png'
+#ICON = 'https://dl.dropboxusercontent.com/u/299446/logo.png'
+#SHORTCUT_ICON = 'https://dl.dropboxusercontent.com/u/299446/logo.png'
